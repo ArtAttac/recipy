8cbf55cf20fdacda721024de5c1e5b1e2276ef88
Arthur Itzhaki <sovietbulldozer@gmail.com>
2020-10-22 03:08:18 -0400

    first commit, recipe finder works

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0978ae93-deea-4402-b3e0-17ded5db6798';
+export const MODAL_CLOSE_SEC = 2.5;
